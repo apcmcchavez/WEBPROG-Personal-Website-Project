0b8c66bf2911b43ef65402674ec335ab5111b4c2
Iyah <147396392+apcmcchavez@users.noreply.github.com>
2025-01-12 18:45:26 +0800

    Delete js directory

--- js/myscript.js
+++
@@ -1,8 +0,0 @@
-// JavaScript for Mobile Menu (Navigation bar)
-
-        const navToggle = document.getElementById('nav-toggle');
-        const navMenu = document.getElementById('nav-menu');
-
-        navToggle.addEventListener('click', () => {
-            navMenu.classList.toggle('show-menu');
-        });
