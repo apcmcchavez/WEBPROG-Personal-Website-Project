d7c05c95b4061c8c770ad23b602836d325f2d09c
Iyah <147396392+apcmcchavez@users.noreply.github.com>
2025-01-12 18:45:54 +0800

    Create myscript.js

---
+++ home/js/myscript.js
@@ -0,0 +1,8 @@
+// JavaScript for Mobile Menu (Navigation bar)
+
+        const navToggle = document.getElementById('nav-toggle');
+        const navMenu = document.getElementById('nav-menu');
+
+        navToggle.addEventListener('click', () => {
+            navMenu.classList.toggle('show-menu');
+        });
